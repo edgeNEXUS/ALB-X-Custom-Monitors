97f8c69736d6a0281b0e14e7f29347b361ce54e1
macrokernel <ssiresking@gmail.com>
2021-07-28 17:03:57 +0100

    Fix LDAP connect

--- EdgeNexus_Perl_LDAPS_healthcheck.pl
+++ EdgeNexus_Perl_LDAPS_healthcheck.pl
@@ -12,29 +12,30 @@ use MIME::Base64;
 # See https://appstore.edgenexus.io/user-guides/user-guide-4-2-x/software-version-4-2-x-user-guide/real-server-monitoring/#Custom_Monitors for more details
 sub monitor
 {
-	 my $host = $_[0];   ### Host IP or name
-	 my $port = $_[1];   ### Host port
-	 my $notes = $_[3];  ### Taken from notes field on Real Server
-	 my $user = $_[5];   ### User field from GUI Library > Real Server Monitors
-	 my $password = $_[6];   ### Password field from GUI Library > Real Server Monitors
-	 
-	 #Password is Base64 encoded as such please passwordd
-         my $passwordd = decode_base64($password);
-	 
- 	 ## Connect and bind to the server.
-	 my $ldap = Net::LDAP->new($host, port => $port, version => 3 )
-	 or return(2);
-		 
-	 my $result = $ldap->start_tls(  );
-	 return(2) if $result->code(  );
-		 
-	 $result = $ldap->bind($user, password => $passwordd);
-	 return(2) if $result->code(  );
+	my $host = $_[0];   ### Host IP or name
+	my $port = $_[1];   ### Host port
+	my $notes = $_[3];  ### Taken from notes field on Real Server
+	my $user = $_[5];   ### User field from GUI Library > Real Server Monitors
+	my $password = $_[6];   ### Password field from GUI Library > Real Server Monitors
+
+	if (!defined($port) || ($port eq '')) {
+	    $port = 636;
+	}
+
+	# Password is Base64 encoded as such please passwordd
+        my $passwordd = decode_base64($password);
+
+ 	# Connect to the server
+	my $ldap = Net::LDAP->new("ldaps://$host", port => $port, version => 3)
+	    or return(23);
+
+	# Bind to the server
+	my $result = $ldap->bind($user, password => $passwordd);
+	return(2) if $result->code();
       
-   	 ## Unbind and exit.
-	 $ldap->unbind(  );
-	 print "LDAP bind Successful.\n";
-	 return(1);
-	 
+   	# Unbind and exit
+	$ldap->unbind();
+	print "LDAP bind Successful.\n";
+	return(1);
 }
 monitor(@ARGV);
